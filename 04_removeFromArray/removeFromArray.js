25ba7cb4fc22d57ba5b948325948feef5baef0aa
Munaaelmi <munaaelmi@outlook.com>
2025-03-01 11:03:15 +0000

    Have removed arguments in from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let removedNumbers = [];
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      removedNumbers.push(item);
+    }
+  });
+  return removedNumbers;
 };
 
 // Do not edit below this line
