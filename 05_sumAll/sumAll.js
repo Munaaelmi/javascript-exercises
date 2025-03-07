b01a6cef5a0f1b5355735202b6d58d8407b23164
Munaaelmi <munaaelmi@outlook.com>
2025-03-07 12:53:08 +0000

    Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let highestNumber = Math.max(num1, num2);
+  let smallestNumber = Math.min(num1, num2);
 
+  let sum = 0;
+
+  if (Number.isInteger(num1) && Number.isInteger(num2)) {
+    if (highestNumber && smallestNumber < 0) {
+      return "ERROR";
+    } else {
+      for (let i = smallestNumber; i <= highestNumber; i++) {
+        sum += i;
+      }
+      return sum;
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
-// Do not edit below this line
 module.exports = sumAll;
