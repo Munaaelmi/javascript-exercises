472348ac70a69d6f1aba1a52365ba6514fe2cc99
Munaaelmi <munaaelmi@outlook.com>
2025-04-12 13:58:17 +0100

    Working through the palindromes challenges

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  if (str.includes("!")) {
+    let punctuation = str.slice(-1);
+    let newWord = str.split("").reverse().join("").slice(1) + punctuation;
+    return newWord.toLowerCase() == str.toLowerCase();
+  }
 
+  return str === str.split("").reverse().join("");
 };
 
 // Do not edit below this line
