eac60eb2647c863f4a7e79fab1cf1b074bedd3ed
Munaaelmi <munaaelmi@outlook.com>
2025-04-13 11:54:20 +0100

    Finished the fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = Number(n);
 
+  let previousNumber = 0;
+  let currentNumber = 1;
+
+  if (n < 0) return "OOPS";
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  for (let i = 2; i <= n; i++) {
+    const nextNumber = previousNumber + currentNumber;
+    previousNumber = currentNumber;
+    currentNumber = nextNumber;
+  }
+
+  return currentNumber;
 };
 
 // Do not edit below this line
